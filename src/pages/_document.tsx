8b0faf1eeb4a20095a3076905a14d82dba6a8750
pedroforadori <foradorif@gmail.com>
2022-12-22 02:37:40 -0300

    ajuste plano de fundo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Code+Latin:wght@400;500;600&display=swap" rel="stylesheet" />
       </Head>
-      <body className="bg-app h-auto bg-cover bg-center bg-no-repeat bg-fixed overflow-x-hidden">
+      <body className="bg-app bg-cover bg-center bg-no-repeat bg-fixed overflow-x-hidden">
         <Main />
         <NextScript />
       </body>
